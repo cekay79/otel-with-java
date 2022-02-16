2c5e3e312e2fae05c4329255051f081b95f30f6a
b9mu5z <christian.kuester@deutschepost.de>
2022-02-16 15:08:38 +0100

    micrometer

--- src/main/java/com/riferrei/otel/java/HelloAppController.java
+++ src/main/java/com/riferrei/otel/java/HelloAppController.java
@@ -1,5 +1,7 @@
 package com.riferrei.otel.java;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +12,15 @@ public class HelloAppController {
     private static final Logger log =
         LoggerFactory.getLogger(HelloAppController.class);
 
+    static final Counter counter = Metrics.counter("test.counter");
+
     @RequestMapping(method= RequestMethod.GET, value="/hello")
     public Response Car() {
         Response response = buildResponse();
         if (response.isValid()) {
             log.info("The response is valid.");
         }
+        counter.increment();
         return response;
     }
 
